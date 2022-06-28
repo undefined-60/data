45c72218d7bb7284681d5f6b11580e4a25a2fd8e
undefined-60 <daniel.xn60@gmail.com>
2022-06-28 06:27:08 -0300

    First commit

---
+++ upload.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+git config --global user.email "daniel.xn60@gmail.com" && git config --global user.name "undefined-60" && git add . && git commit -m "First commit" && git push origin master && undefined-60 && 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
